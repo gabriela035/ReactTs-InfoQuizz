3a3621b5c6713142cf5bde7023b686fdc74b7388
gabriela035 <szuiogangabriela@gmail.com>
2025-07-16 16:42:20 +0300

    project structure

--- info-quiz/src/main.tsx
+++ info-quiz/src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { BrowserRouter } from "react-router-dom";
+//import { QuizProvider } from "./context/QuizContext";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      {/* <QuizProvider> */}
+      <App />
+      {/* </QuizProvider> */}
+    </BrowserRouter>
+  </StrictMode>
+);
